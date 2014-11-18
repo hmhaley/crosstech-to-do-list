8d9f8c5db2bf34e60de0f00400f8b57ee16ebe7c
hmhaley <mhaley@vmgstrategies.com>
2014-11-18 09:47:21 -0800

    Initialize repo - first commit for to-do-list

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToDoList::Application.config.secret_key_base = '8611e3591c4e41828c62459156641de106e780ad445fb350ebbd21a7dce83b67517490872382ec69bc34a6196b5892065edfbae449efc30d2e4e33e5ac9a2f91'
