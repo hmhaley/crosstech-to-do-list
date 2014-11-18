5ca42993356ce0e71a3b69ee1d2372af773ee33f
hmhaley <mhaley@vmgstrategies.com>
2014-11-18 12:09:34 -0800

    fixed the routes.rb one more time

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ root 'home#index'
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
